8d4bd4893c748f0ab5eeaf038f3c231d26b7fcd6
stefanstepashka <bidzhamov.stevens@mail.ru>
2023-05-06 14:41:25 -0700

    urls

--- taskproject/task/urls.py
+++ taskproject/task/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from . import views
 from . import api
 from rest_framework.routers import DefaultRouter
+from django.contrib.auth import views as auth_views
+from .views import register_view
 router = DefaultRouter()
 router.register(r'tasks', api.TaskViewSet)
 
@@ -11,5 +13,8 @@ urlpatterns = [
     path('create/', views.task_create, name='task_create'),
     path('update/<int:task_id>/', views.task_update, name='task_update'),
     path('delete/<int:task_id>/', views.task_delete, name='task_delete'),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('register/', register_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
